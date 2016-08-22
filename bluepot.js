0828d3bdb18e6b10291327007f4d6504c61deca3
Tim Nyborg <tim.nyborg@gmail.com>
2016-08-22 11:02:21 +0100

    Add files via upload

---
+++ bluepot.js
@@ -0,0 +1,10 @@
+// Define a global bluepot object that will be used on all bluepot pages
+console.log('Bluepot.js loaded');
+var bluepot={
+    "API_URL":'/bluepot/api/',
+    
+    "current_page": {} // used to store local variables on a page
+    
+    //"modal_form_handler": function // handler function called by modal form when the "save button is pressed 
+    
+};
